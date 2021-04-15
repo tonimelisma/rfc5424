5ec448e9f96098794aa903e88c183ab558b29e2d
Toni Melisma <toni.melisma@iki.fi>
2021-04-15 12:07:53 +0300

    Add example

--- example_test.go
+++ example_test.go
@@ -1,13 +1,13 @@
-# rfc5424 [![GoDoc](https://godoc.org/github.com/tonimelisma/rfc5424?status.svg)](https://pkg.go.dev/mod/github.com/tonimelisma/rfc5424) [![Go Report Card](http://goreportcard.com/badge/tonimelisma/rfc5424)](http://goreportcard.com/report/tonimelisma/rfc5424) ![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/tonimelisma/rfc5424) ![License](https://img.shields.io/badge/license-MIT-blue.svg)
-Fast RFC5424 syslog message parser written in Go
+package rfc5424_test
 
-## Usage
+import (
+	"bytes"
+	"fmt"
 
-```go get github.com/tonimelisma/rfc5424```
+	"github.com/tonimelisma/rfc5424"
+)
 
-## Example
-
-```func ExampleParseMultiple() {
+func ExampleParseMultiple() {
 	testMessageBuffer := []byte(`83 <40>1 2012-11-30T06:45:29+00:00 host app web.3 - State changed from starting to up
 119 <40>1 2012-11-30T06:45:26+00:00 host app web.3 - Starting process with command 'bundle exec rackup config.ru -p 24405'
 `)
@@ -35,17 +35,3 @@ Fast RFC5424 syslog message parser written in Go
 	// 2012-11-30T06:45:29+00:00 [syslog.emerg] host app web.3: State changed from starting to up
     // 2012-11-30T06:45:26+00:00 [syslog.emerg] host app web.3: Starting process with command 'bundle exec rackup config.ru -p 24405'
 }
-```
-
-For a practical example that parses syslog messages from HTTPS POST bodies as a 
-log drain for Heroku, see [https://github.com/tonimelisma/golang-heroku-log-drain](https://github.com/tonimelisma/golang-heroku-log-drain)
-
-## Caveats
-
-RFC5424 defines a way to transmit structured data messages in addition to the more typical free-form text log messages.
-This structured data is not parsed in any way. I've never actually seen any system use the data.
-
-## Maintenance
-
-Although I will not update the software, I will attempt to answer opened
-issues or PRs in a reasonable timeframe.
